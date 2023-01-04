c512b9b027571c1b571db57b4633a93fe2a6f47e
Guilherme Camargo <guisfits@hotmail.com>
2023-01-03 21:02:11 -0300

    user struct and accounts context

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,27 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+    The Account context
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "José", username: "josevalim"},
+      %User{id: "2", name: "Bruce", username: "redrapids"},
+      %User{id: "2", name: "Chris", username: "chrismccord"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), & &1.id == id)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} ->
+        Map.get(map, key) == val
+      end)
+    end)
+  end
+end
