44d006b17275d6f84726748b968e963d933aa3ce
Guilherme Camargo <guisfits@hotmail.com>
2023-01-10 22:00:30 -0300

    session controller with login logic

---
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,25 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+
+  alias Rumbl.Accounts
+  alias RumblWeb.Plugs.Auth
+
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => password}}) do
+    case Accounts.authenticate_by_username_and_pass(username, password) do
+      {:ok, user} ->
+        conn
+        |> Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+end
