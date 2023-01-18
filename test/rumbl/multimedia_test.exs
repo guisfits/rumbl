899817f33e55f25645cf793ba3e07a7be2ee9690
Guilherme Camargo <guisfits@hotmail.com>
2023-01-17 21:33:57 -0300

    multimedia test

--- test/rumbl/multimedia_test.exs
+++ test/rumbl/multimedia_test.exs
@@ -2,40 +2,53 @@ defmodule Rumbl.MultimediaTest do
   use Rumbl.DataCase
 
   alias Rumbl.Multimedia
+  alias Rumbl.Multimedia.Video
+  alias Rumbl.Accounts.User
 
-  describe "videos" do
-    alias Rumbl.Multimedia.Video
+  import Rumbl.MultimediaFixtures
 
-    import Rumbl.MultimediaFixtures
+  @invalid_attrs %{description: nil, title: nil, url: nil}
 
-    @invalid_attrs %{description: nil, title: nil, url: nil}
-
-    test "list_videos/0 returns all videos" do
+  describe "list_videos/0" do
+    test "returns all videos" do
       video = video_fixture()
       assert Multimedia.list_videos() == [video]
     end
+  end
 
-    test "get_video!/1 returns the video with given id" do
+  describe "get_video!/1" do
+    test "returns the video with given id" do
       video = video_fixture()
       assert Multimedia.get_video!(video.id) == video
     end
+  end
 
-    test "create_video/1 with valid data creates a video" do
+  describe "create_video/1" do
+    test "with valid data creates a video" do
+      user = user_fixture()
       valid_attrs = %{description: "some description", title: "some title", url: "some url"}
 
-      assert {:ok, %Video{} = video} = Multimedia.create_video(valid_attrs)
+      assert {:ok, %Video{} = video} = Multimedia.create_video(user, valid_attrs)
       assert video.description == "some description"
       assert video.title == "some title"
       assert video.url == "some url"
     end
 
-    test "create_video/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Multimedia.create_video(@invalid_attrs)
+    test "with invalid data returns error changeset" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Multimedia.create_video(user, @invalid_attrs)
     end
+  end
 
-    test "update_video/2 with valid data updates the video" do
+  describe "update_video/2" do
+    test "with valid data updates the video" do
       video = video_fixture()
-      update_attrs = %{description: "some updated description", title: "some updated title", url: "some updated url"}
+
+      update_attrs = %{
+        description: "some updated description",
+        title: "some updated title",
+        url: "some updated url"
+      }
 
       assert {:ok, %Video{} = video} = Multimedia.update_video(video, update_attrs)
       assert video.description == "some updated description"
@@ -43,21 +56,36 @@ defmodule Rumbl.MultimediaTest do
       assert video.url == "some updated url"
     end
 
-    test "update_video/2 with invalid data returns error changeset" do
+    test "with invalid data returns error changeset" do
       video = video_fixture()
       assert {:error, %Ecto.Changeset{}} = Multimedia.update_video(video, @invalid_attrs)
       assert video == Multimedia.get_video!(video.id)
     end
+  end
 
-    test "delete_video/1 deletes the video" do
+  describe "delete_video/1" do
+    test " deletes the video" do
       video = video_fixture()
       assert {:ok, %Video{}} = Multimedia.delete_video(video)
       assert_raise Ecto.NoResultsError, fn -> Multimedia.get_video!(video.id) end
     end
+  end
 
-    test "change_video/1 returns a video changeset" do
+  describe "change_video/1" do
+    test " returns a video changeset" do
       video = video_fixture()
       assert %Ecto.Changeset{} = Multimedia.change_video(video)
+    end
+  end
+
+  describe "list_user_videos/1" do
+    test "returns videos associated with the user" do
+      user = user_fixture()
+      video1 = video_fixture(user_id: user.id)
+      video2 = video_fixture(user_id: user.id)
+      video_fixture(user_id: user.id + 1)
+
+      assert Multimedia.list_user_videos(user) == [video1, video2]
     end
   end
 end
--- test/support/fixtures/multimedia_fixtures.ex
+++ test/support/fixtures/multimedia_fixtures.ex
@@ -1,11 +1,11 @@
 defmodule Rumbl.MultimediaFixtures do
   @moduledoc """
-  This module defines test helpers for creating
-  entities via the `Rumbl.Multimedia` context.
+    This module defines test helpers for creating
+    entities via the `Rumbl.Multimedia` context.
   """
 
   @doc """
-  Generate a video.
+    Generate a video.
   """
   def video_fixture(attrs \\ %{}) do
     {:ok, video} =
@@ -18,5 +18,22 @@ defmodule Rumbl.MultimediaFixtures do
       |> Rumbl.Multimedia.create_video()
 
     video
+  end
+
+  @doc """
+    Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        name: "Guilherme",
+        username: "guisfits",
+        password: "1234abc",
+        password_hash: "1234abc",
+      })
+      |> Rumbl.Accounts.create_user()
+
+    user
   end
 end
