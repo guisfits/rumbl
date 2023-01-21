9b8abc9503633062ae20ad769b142c58ae672a08
Guilherme Camargo <guisfits@hotmail.com>
2023-01-21 17:54:05 -0300

    creating slugs

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -5,10 +5,12 @@ defmodule Rumbl.Multimedia.Video do
   alias Rumbl.Multimedia.Category
   alias Rumbl.Accounts.User
 
+  @primary_key {:id, Rumbl.Multimedia.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
 
     belongs_to :user, User
     belongs_to :category, Category
@@ -22,5 +24,19 @@ defmodule Rumbl.Multimedia.Video do
     |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
     |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} -> put_change(changeset, :slug, slugify(new_title))
+      :error -> changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
