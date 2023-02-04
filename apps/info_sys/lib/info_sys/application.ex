3132bc635918de6c927da4178b7c5fe3099378e7
Guilherme Camargo <guisfits@gmail.com>
2023-02-04 19:24:10 -0300

    cache

--- apps/info_sys/lib/info_sys/application.ex
+++ apps/info_sys/lib/info_sys/application.ex
@@ -1,6 +1,4 @@
 defmodule InfoSys.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,10 @@ defmodule InfoSys.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: InfoSys.Worker.start_link(arg)
-      # {InfoSys.Worker, arg}
+      InfoSys.Cache,
+      {Task.Supervisor, name: InfoSys.TaskSupervisor}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
     Supervisor.start_link(children, opts)
   end
