da3511951496e16b0485e1a344bea399e8014cb1
Guilherme Camargo <guisfits@gmail.com>
2023-02-11 14:57:57 -0300

    info_sys test

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,47 @@
 defmodule InfoSysTest do
-  use ExUnit.Case
-  doctest InfoSys
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert InfoSys.hello() == :world
+  alias InfoSys.Result
+
+  defmodule TestBackend do
+    def name(), do: "Wolfram"
+
+    def compute("result", _opts) do
+      [%Result{backend: __MODULE__, text: "result"}]
+    end
+
+    def compute("none", _opts) do
+      []
+    end
+
+    def compute("timeout", _opts) do
+      Process.sleep(:infinity)
+    end
+
+    def compute("boom", _opts) do
+      raise "boom!"
+    end
+  end
+
+  describe "compute/2" do
+    test "with results" do
+      response = InfoSys.compute("result", backends: [TestBackend])
+      assert response == [%Result{backend: TestBackend, text: "result"}]
+    end
+
+    test "with no backend results" do
+      response = InfoSys.compute("none", backends: [TestBackend])
+      assert response == []
+    end
+
+    test "with timeout returns no result" do
+      response = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+      assert response == []
+    end
+
+    @tag :capture_log
+    test "with exception returns no result" do
+      assert InfoSys.compute("boom", backends: [TestBackend]) == []
+    end
   end
 end
