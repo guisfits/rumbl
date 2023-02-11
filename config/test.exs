a36c9595fdb06867d57574f6a4b33739e4e7322c
Guilherme Camargo <guisfits@gmail.com>
2023-02-11 15:53:53 -0300

    testing wolfram

--- config/test.exs
+++ config/test.exs
@@ -27,3 +27,7 @@ config :logger, level: :warn
 config :phoenix, :plug_init_mode, :runtime
 
 config :pbkdf2_elixir, :rounds, 1
+
+config :info_sys, :wolfram,
+  app_id: "1234",
+  http_client: InfoSys.Test.HttpClient
